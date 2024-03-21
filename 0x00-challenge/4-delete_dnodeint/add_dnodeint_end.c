514a0d0cb5bb8c489f9cf367d788b63838ec5456
Lawson Pascal Israel <isreallawson@outlook.com>
2024-03-21 19:32:34 +0000

    back to old version

--- 0x00-challenge/4-delete_dnodeint/add_dnodeint_end.c
+++ 0x00-challenge/4-delete_dnodeint/add_dnodeint_end.c
@@ -24,8 +24,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new->next = NULL;
 	if (*head == NULL)
 	{
-		new->prev = NULL;
 		*head = new;
+		new->prev = NULL;
 		return (new);
 	}
 	l = *head;
